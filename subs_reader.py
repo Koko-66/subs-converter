f1019699ada139a7160ad5cb599886a3aaea30a4
Koko <pointmisser@gmail.com>
2023-10-16 23:08:00 +0100

    Add counting formulas.
    Add LEN and COUNT to columns, formatting to header. Remove print statements and tidy up.

--- subs_reader.py
+++ subs_reader.py
@@ -1,7 +1,8 @@
 # %%
 import os
-import validators
 from openpyxl import Workbook
+import validators
+from formatting import format_header
 
 
 """
@@ -38,20 +39,24 @@ def get_path_to_folder():
 
 
 def read_files(file_dir):
+    """Read file content and prepare for saving to Excel"""
     for root, dirs, files in os.walk(file_dir):
         for file in files:
+            # Check if file is a subtitle file
             if file.split(".")[-1] in ["vtt", "srt"]:
                 file_path = os.path.join(root, file)
-                # print(file)
-                file_content = []
+
                 with open(file_path, 'r', encoding='utf-8') as file:
                     file_content = [line for line in file]
-                    print(file_content)
-                    # for i, text in enumerate(file_content):
+
                     with_counts = [[
-                        text, '', f'=LEN(A{i+1})', f'=LEN(B{i+1})', f'=IF(LEN(B{i+1})>LEN(A{i+1}),"Too long", "Ok")'] for i, text in enumerate(file_content)]
-                    print(with_counts)
+                        text, '',
+                        f'=LEN(A{i+1})',
+                        f'=LEN(B{i+1})',
+                        f'=IF(LEN(B{i+1})>LEN(A{i+1}),"Too long", "Ok")'] for i, text in enumerate(file_content, 1)]
+
                     save_file_list_to_excel(with_counts, file)
+
 
 
 def save_file_list_to_excel(content, file):
@@ -59,7 +64,10 @@ def save_file_list_to_excel(content, file):
     wb = Workbook()
     ws = wb.active
     wb.create_sheet()
+    
     ws.append(["Source", "Translation", "Source count", "Target count"])
+    format_header(ws)
+    
     for row in content:
         ws.append(row)
 
@@ -67,8 +75,6 @@ def save_file_list_to_excel(content, file):
 
 
 file_dir = get_path_to_folder()
-# subs_content = read_files(file_dir)
 read_files(file_dir)
-# save_file_list_to_excel(subs_content)
 
 # %%
